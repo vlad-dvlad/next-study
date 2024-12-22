cb00828663e37904f8775ba4c9c5721cb203b965
Vladyslav Ivashko <vladyslavivashko@Vladyslavs-MacBook-Air.local>
2024-12-23 00:21:40 +0200

    finish 5 chapters

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <p>
+          Customers Page  
+        </p>
+    );
+};
+
+export default Page;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <p>
+          Dashboard page  
+        </p>
+    );
+};
+
+export default Page;
