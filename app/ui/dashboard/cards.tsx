6c78a80e6ad8f077113ed2068f0111c92574f077
Vladyslav Ivashko <vladyslavivashko@Vladyslavs-MacBook-Air.local>
2024-12-24 00:45:27 +0200

    ch 6-7

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -16,8 +16,6 @@ const iconMap = {
 export default async function CardWrapper() {
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
       {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
